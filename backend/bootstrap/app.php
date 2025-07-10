d7ce8d8452ee49a95bf30794cb2836d5455f01f1
Praestocode <143253397+Praestocode@users.noreply.github.com>
2025-07-11 00:16:01 +0200

    Update app.php

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -13,9 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->api(prepend: [
-            Cors::class,
-        ]);
+        $middleware->appendToGroup('api', \App\Http\Middleware\Cors::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
