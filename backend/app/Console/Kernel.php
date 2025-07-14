ca9edc00dc7a4822f67220017f256ea96e1ca5f8
Praestocode <143253397+Praestocode@users.noreply.github.com>
2025-07-14 23:31:21 +0200

    Update Kernel.php
    BETA VERSION - Test per il cronjob

--- backend/app/Console/Kernel.php
+++ backend/app/Console/Kernel.php
@@ -4,6 +4,7 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\Log;
 
 class Kernel extends ConsoleKernel
 {
@@ -14,6 +15,7 @@ class Kernel extends ConsoleKernel
 
     protected function schedule(Schedule $schedule)
     {
+        \Log::info('Schedule command check at '.now().' timezone: '.config('app.timezone'));
         $schedule->command('smoke:process-daily')->everyMinute();
         $schedule->command('tasks:process-daily')->everyMinute();
     }
